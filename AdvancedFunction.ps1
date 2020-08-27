77a4c0441493fc953666bb9959a8297e4362dc9d
Sheri98 <52729173+Sheri98@users.noreply.github.com>
2020-08-27 11:30:32 +0530

    Add files via upload

---
+++ AdvancedFunction.ps1
@@ -0,0 +1,13 @@
+﻿function Show-AdvancedScript
+{
+        [CmdletBinding( SupportsShouldProcess = $True)]
+        param( 
+        [parameter()]
+        $FilePath
+        )
+        Write-Verbose "Deleted $FilePath" 
+        if($PSCmdlet.ShouldProcess(("Deleting File Permanently","$FilePath" ))){
+        
+        Remove-Item $FilePath
+        }
+}
---
+++ Function.ps1
@@ -0,0 +1,15 @@
+﻿#function Add ( $num1 , $num2 )
+#{
+#$num3 = $num1 + $num2;
+#Write-Output $num3
+#}
+function FLiping ( $num1 , $num2 , [switch]$flip )
+{
+
+if ( $flip ) {
+$num3 = $num1 - $num2;
+Write-Output $num3;
+}
+$num3 = $num1 + $num2;
+Write-Output $num3;
+}
